4d37c9b22333cc013c9f9e474aa4e24ecf665a75
danieljj28 <danijj28@gmail.com>
2024-05-02 11:41:17 +0200

    creando carpeta docs para subir a githubPages

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -8,7 +8,7 @@ export class GifsService {
   public gifList: Gif[] = [];
 
   private _tagsHistory: string[] = [];
-  private apiKey:       string = 'dGMJW12nlHSz46O3sy81BZFryoPshmUk';
+  private apiKey:       string = 'TPB7FN4HE7lScXBHZGgQ38is01jIk1TQ';
   private serviceUrl:   string = 'https://api.giphy.com/v1/gifs';
 
   constructor( private http: HttpClient ) {
